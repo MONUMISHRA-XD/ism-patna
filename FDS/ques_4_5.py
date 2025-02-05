c304881234bbfef918be3b12ceabc2e6cfd3415b
MONU-GAMER <84650793+MONU-GAMER@users.noreply.github.com>
2025-02-06 00:05:48 +0530

    Create ques_4_5.py

---
+++ FDS/ques_4_5.py
@@ -0,0 +1,24 @@
+
+# Find the Sum of the Series 1 + 2 + 3 + 4 + ... + 50 in Python or C?
+def one_line_fn():
+    print(sum(range(1,51)))
+def usingloop():
+    sum = 0
+    for i in range(1,51):
+        sum =sum+i
+    print(sum)
+
+def using_math_formula():
+    #formula  Sn = n/2(2*a+(n-1)*d)
+    #formula Sn = n/2*(first+last)
+    sum = 50/2*(1+50)
+    print(sum)
+#-----------------------------------------------------------------------------------------------------------------------------------
+#Find the Sum of the Series 1²  + 2²  + 3²  + 4²  + ... + 50²  in Python?
+    
+
+def usingloop_sq():
+    total=0
+    for i in range(1,51):
+        total = total+i**2  
+    print(total)
