98d224b9c6d0f92bbe5ec95f383b547e290f7aff
MONU-GAMER <84650793+MONU-GAMER@users.noreply.github.com>
2025-02-07 23:29:37 +0530

    Create 7feb.py

---
+++ FDS/7feb.py
@@ -0,0 +1,23 @@
+list = [0,1,2,3,4,5,6]
+#for i in range(10):
+#    data = int(input("enter the data : "))
+#   list.append(data)
+print(list)
+evenlist = [0,2,4,6,8,10]
+index = int(input("""
+press 1 for change even index
+press 2 for change odd index
+"""))
+if index==1:
+    even = int(input(f"choose index you want to  add data upto {len(list)}:"))
+    data = int(input("enter the data :"))
+    list[even]=list[data]
+    
+    
+
+else:
+
+
+print(list)
+
+
